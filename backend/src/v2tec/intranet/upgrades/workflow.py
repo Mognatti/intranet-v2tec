6c681c223f1e56a862a23b89053bcded96ebc0ed
Caio Mognatti <caiocsm97@gmail.com>
2026-05-28 17:12:32 -0300

    ex 21: upgrade step for area type

--- backend/src/v2tec/intranet/upgrades/workflow.py
+++ backend/src/v2tec/intranet/upgrades/workflow.py
@@ -4,7 +4,7 @@ from Products.GenericSetup.tool import SetupTool
 from v2tec.intranet import logger
 
 
-def upgrade_permission(portal_setup: SetupTool):
+def atualiza_permissoes(portal_setup: SetupTool):
     """Upgrade all permissions follwing the new workflow."""
     message = "Permissões de workflow atualizadas seguindo o novo workflow."
     portal = api.portal.get()
