6fe6e4b471d4009c4f4d93f1d8c1f1bb5969c2b7
Caio Mognatti <caiocsm97@gmail.com>
2026-05-28 16:14:13 -0300

    ex 18: fechar a intranet

---
+++ backend/src/v2tec/intranet/upgrades/workflow.py
@@ -0,0 +1,12 @@
+from plone import api
+from Products.CMFPlone.WorkflowTool import WorkflowTool
+from Products.GenericSetup.tool import SetupTool
+from v2tec.intranet import logger
+
+
+def upgrade_permission(portal_setup: SetupTool):
+    """Upgrade all permissions follwing the new workflow."""
+    message = "Permissões de workflow atualizadas seguindo o novo workflow."
+    wf_tool: WorkflowTool = api.portal.get_tool("portal_workflow")
+    wf_tool.updateRoleMappings()
+    logger.info(message)
