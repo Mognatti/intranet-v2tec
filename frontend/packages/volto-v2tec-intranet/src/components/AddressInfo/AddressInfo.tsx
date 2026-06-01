aee237417905e0bd532a7cffbae8b2273668e71c
Caio Mognatti <caiocsm97@gmail.com>
2026-06-01 16:04:10 -0300

    ex 27 - adiciona mais campos nas areas (front)

---
+++ frontend/packages/volto-v2tec-intranet/src/components/AddressInfo/AddressInfo.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Container } from '@plone/components';
+import type { Area } from 'volto-v2tec-intranet/types/content';
+
+interface AddressInfoProps {
+  content: Area;
+}
+
+const AddressInfo: React.FC<AddressInfoProps> = ({ content }) => {
+  const { endereco, complemento, cidade, estado, cep } = content;
+
+  if (!endereco && !complemento && !cidade && !estado && !cep) {
+    return null;
+  }
+
+  return (
+    <Container narrow className="endereco">
+      {endereco && (
+        <Container className="endereco-linha">
+          <span className="label">Endereço</span>:
+          <span className="value">{endereco}</span>
+        </Container>
+      )}
+      {complemento && (
+        <Container className="endereco-linha">
+          <span className="label">Complemento</span>:
+          <span className="value">{complemento}</span>
+        </Container>
+      )}
+      {cidade && (
+        <Container className="endereco-linha">
+          <span className="label">Cidade</span>:
+          <span className="value">{cidade}</span>
+        </Container>
+      )}
+      {estado && (
+        <Container className="endereco-linha">
+          <span className="label">Estado</span>:
+          <span className="value">{estado}</span>
+        </Container>
+      )}
+      {cep && (
+        <Container className="endereco-linha">
+          <span className="label">CEP</span>:
+          <span className="value">{cep}</span>
+        </Container>
+      )}
+    </Container>
+  );
+};
+
+export default AddressInfo;
